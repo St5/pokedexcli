7aab4c9cf564e86372f1e6f3f429ca25b1fd33dd
Oleksii <oleksii.pakholkov@tvgmedia.com>
2024-10-29 23:16:46 +0200

    first commit

---
+++ command_help.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func callbackHelp() error {
+	fmt.Println("Welcome to the Pkedex")
+	fmt.Println("List of available commands:")
+	for _, cmd := range getCommands() {
+		fmt.Printf(" - %s , %s \n", cmd.name, cmd.description)
+	}
+	fmt.Println(" ")
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/st5/pokedexcli
+
+go 1.23.2
